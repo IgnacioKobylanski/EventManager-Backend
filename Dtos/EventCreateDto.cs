94a5d89f1f5ff42f1f6831763749e255eff56017
IgnacioKobylanski <ignaciojk@outlook.com.ar>
2025-07-31 19:06:40 -0300

    minor changes in Event model

---
+++ Dtos/EventCreateDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EventManager.Dtos
+{
+    public class EventCreateDto
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public string Location { get; set; } = null!;
+        public string? ImageUrl { get; set; }
+        public int? Capacity { get; set; }
+        [Required]
+        public int UserId { get; set; }
+    }
+}
